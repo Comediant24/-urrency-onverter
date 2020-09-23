92b7a4fd804ed26223f5e9297876d07778f32dc0
Iurii Bairamukov <juribai@mail.ru>
2020-09-23 10:19:58 +0700

    feat: подключть api и конфиг в Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
+import api from '../utils/api';
 import Input from './Input';
+import { value } from '../utils/currentApi';
 
 function Main() {
+  const [currencies, setCurrencies] = React.useState(null);
+
+  React.useEffect(() => {
+    api.getCurrency().then((data) => {
+      setCurrencies(data.rates);
+    });
+  }, []);
+
   return (
     <main className="container">
       <form name="search" className="form form_type_search">
         <h1 className="form__title">Какая пара валют?</h1>
         <div className="form__section">
           <div className="form__wrapper form__wrapper_type_left">
-            <Input inputType="form__input_left" />
+            <Input
+              inputType="form__input_left"
+              currencies={currencies}
+              value={value[0]}
+            />
           </div>
           <div className="form__wrapper">
-            <Input inputType="form__input_right" />
+            <Input
+              inputType="form__input_right"
+              currencies={currencies}
+              value={value[1]}
+            />
           </div>
         </div>
       </form>
