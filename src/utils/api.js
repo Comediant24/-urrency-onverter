e909f21197b2dbe1635d692e8f6fef1b32ebb80d
Iurii Bairamukov <juribai@mail.ru>
2020-09-23 10:18:07 +0700

    feat: добавить api и конфиг валют

---
+++ src/utils/api.js
@@ -0,0 +1,24 @@
+import {
+  currenciesApi
+} from "./currentApi";
+
+class Api {
+  constructor(urlApi) {
+    this._url = urlApi;
+  }
+
+  _handleResponse(response) {
+    if (response.ok) {
+      return response.json();
+    }
+    return Promise.reject(`Ошибка: ${response.status}`);
+  }
+
+  getCurrency() {
+    return fetch(`${this._url}`).then((res) => this._handleResponse(res));
+  }
+}
+
+const api = new Api(currenciesApi);
+
+export default api
---
+++ src/utils/currentApi.js
@@ -0,0 +1,3 @@
+export const value = ['USD', 'RUB', 'THB']
+
+export const currenciesApi = `http://data.fixer.io/api/latest?access_key=2a2fa60f4a43b0671ca7f0f2e3bda31a&symbols=${value}`
