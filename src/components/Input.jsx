887f197c690c25f72ce0c648f0cebe5fa4543c19
Iurii Bairamukov <juribai@mail.ru>
2020-09-23 15:58:46 +0700

    feat: перенести select в Input удалить Currencies

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
-import Currencies from './Currencies';
 
 function Input({ inputType, currencies, value }) {
+  const [inputValue, setInputValue] = React.useState(1);
+  const [selectValue, setSelectValue] = React.useState(value);
+
+  function handleChange(event) {
+    setSelectValue(event.target.value);
+  }
+  function changeInput(evt) {
+    setInputValue(evt.target.value);
+  }
+
   return (
     <>
       <input
@@ -9,9 +18,24 @@ function Input({ inputType, currencies, value }) {
         name="entityId1"
         placeholder="Введите значение"
         className={`form__input ${inputType} form__input_type_entity-id`}
+        value={inputValue}
+        onChange={changeInput}
       />
       <div className="form__select-container">
-        <Currencies currencies={currencies} value={value} />
+        <select
+          value={selectValue}
+          name="currency"
+          onChange={handleChange}
+          className="form__select"
+        >
+          {currencies
+            ? Object.keys(currencies).map((item, index) => (
+                <option key={index} value={item}>
+                  {item}
+                </option>
+              ))
+            : null}
+        </select>
       </div>
     </>
   );
