5ff06c760530bbda9ab4f67757fb8eeb1fc5ce21
Iurii Bairamukov <juribai@mail.ru>
2020-10-06 17:22:04 +0700

    Updates

---
+++ precache-manifest.2ca4ccaeb0682ea464df7418caf103c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbb14d2707234616ae97b8dbd829180f",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "988ae58b458154a72d19",
+    "url": "/currency-converter/static/css/main.241b85a1.chunk.css"
+  },
+  {
+    "revision": "5b4423d74972fe384897",
+    "url": "/currency-converter/static/js/2.9044a336.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-converter/static/js/2.9044a336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "988ae58b458154a72d19",
+    "url": "/currency-converter/static/js/main.891c45da.chunk.js"
+  },
+  {
+    "revision": "a0730edf8381b13d6dc3",
+    "url": "/currency-converter/static/js/runtime-main.46121f8e.js"
+  },
+  {
+    "revision": "8c0656173f2b2eac101330e2f2434026",
+    "url": "/currency-converter/static/media/logo.8c065617.svg"
+  }
+]);
